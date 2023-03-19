d9d8a83dc0cb0400096d8fc33d8a901fb05f6032
Gabriel Alves de Freitas <gabrieltoberdan@hotmail.com>
2023-03-19 19:22:23 -0300

    Testando service-worker de larissa

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,49 @@
-var cacheName = 'pwaTeste+-v1.2';
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js%27');
+
+const CACHE = "pwabuilder-page";
+
+const offlineFallbackPage = "index.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
+
+/*var cacheName = 'pwaTeste+-v1.2';
 
 self.addEventListener('install', event => {
 
@@ -10,13 +55,16 @@ self.addEventListener('install', event => {
 
         './index.html',
 
-        './assets/css/bootstrap.min.css',
+        './assets/css/fontawesome-all.min.css',
+        './assets/css/main.css',
 
         './assets/js/bootstrap.min.js',
 
         './assets/js/jquery.min.js',
 
         './assets/js/popper.min.js',
+
+        './images/hk_48.png',
 
         './assets/img/background.png',
         './assets/img/favicon.png',
@@ -28,6 +76,8 @@ self.addEventListener('install', event => {
         './images/icons/hk_192.png',
         './images/icons/hk_512.png',
         './assets/img/formulas.JPG',
+
+
       ]))
   );
 });
@@ -46,7 +96,7 @@ self.addEventListener('fetch', function (event) {
      } catch (err) {
        return caches.match(event.request);
      }
-   }());
+   }());*/
 
   //Atualizacao cache
   /*event.respondWith(
@@ -59,4 +109,4 @@ self.addEventListener('fetch', function (event) {
       })
   );*/
 
-});
+//});
