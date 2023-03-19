5c1b6e858ec96e491f0b0c15507eaf7ac1b88a25
Gabriel Alves de Freitas <gabrieltoberdan@hotmail.com>
2023-03-19 18:36:09 -0300

    Quarto commit, adicionado icones, manifest e service worker.

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+var cacheName = 'pwaTeste+-v1.2';
+
+self.addEventListener('install', event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+
+        './index.html',
+
+        './assets/css/bootstrap.min.css',
+
+        './assets/js/bootstrap.min.js',
+
+        './assets/js/jquery.min.js',
+
+        './assets/js/popper.min.js',
+
+        './assets/img/background.png',
+        './assets/img/favicon.png',
+        './assets/img/logo.png',
+        './images/icons/hk_48.png',
+        './images/icons/hk_72.png',
+        './images/icons/hk_96.png',
+        './images/icons/hk_144.png',
+        './images/icons/hk_192.png',
+        './assets/img/formulas.JPG',
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+     try {
+       return await fetch(event.request);
+     } catch (err) {
+       return caches.match(event.request);
+     }
+   }());
+
+  //Atualizacao cache
+  /*event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );*/
+
+});
